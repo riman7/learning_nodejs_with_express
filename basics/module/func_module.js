cd3b6ea677352f91c527ea384cba67366070ee20
riman7 <rimanmaharjan7@gmail.com>
2025-01-08 22:36:46 +0545

    changes

--- basics/module/func_module.js
+++ basics/module/func_module.js
@@ -1,4 +1,7 @@
-function sayHi (name) {
+//every file in a node is module
+//note: if we call a function inside a module, the function will be called when you are importing that module in other file using require
+
+sayHi = (name)=> {
     console.log(`Hello ${name}`);
 }
 module.exports = sayHi;
