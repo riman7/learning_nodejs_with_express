86df7449419b412825607867f733c8834f5d04bf
riman7 <r.i.m.a.n.maharjan7@gmail.com>
2025-01-13 22:37:49 +0545

    m

---
+++ basics/npm packages/event_loop.js
@@ -0,0 +1,3 @@
+// Javascipt is blocking, synchronous, single-threaded.(ie.normally, javascript reads everything line by line.)
+//But, Event loop allows non-blocking, asynchronous operations.
+//async methods are stored in libuv during execuation and will be execuated at last
