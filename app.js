4ba38c873236864a0c83fb371a4a32593c4c6953
riman7 <r.i.m.a.n.maharjan7@gmail.com>
2025-01-19 13:51:20 +0545

    app.js

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
 //this callback function will be called everytime user hits our server
-    console.log("user hit the server");
-    res.end('home page')
+    url = req.url;
+    if(url=='/'){
+        res.writeHead(200, {'content-type':'text/html'});
+        res.write('<h1>Hello World</h1>');
+        res.end();
+    }
+    else{
+        res.writeHead(404, {'content-type':'text/html'});
+        res.write('<h1>Page not found</h1>');
+        res.end();
+    }
 });
 server.listen(3000);
-//4:15
+//Visit http://localhost:3000/
